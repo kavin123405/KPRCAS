9571b81a274233e92f4332f4ee422b81b1ff0e2a
kavin <kavinkumaroviyan006@gmail.com>
2025-06-14 16:03:05 +0530

    modified

---
+++ css/java script/script.js
@@ -0,0 +1,30 @@
+var a="10";
+var b=20;
+console.log(a+b)
+
+console.log(a==b);
+console.log(a===b);
+console.log(a!=b); 
+console.log(a!==b);
+       
+var mark=78;
+var result=(mark>=80)? "0":(mark>=60)? "a+":(mark>=35)? "p":"f"
+console.log(result);
+
+if(mark>=80 && mark<=100){
+    console.log("o");
+}
+
+
+console.log(typeof a);
+console.log(typeof b);
+a=true
+console.log(typeof a);
+var c;
+console.log(typeof c);
+var c=null;
+console.log(c);
+
+
+
+
